f5542a817e50056131dec311f7d2ddb3c6930e45
Hishaampp <hishampp198@gmail.com>
2023-10-19 16:26:07 +0530

    hisham

--- lib/createnewaccount.dart
+++ lib/createnewaccount.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-class createnewaccount extends StatelessWidget {
+class createnewaccount extends StatefulWidget {
   const createnewaccount({Key? key}) : super(key: key);
+
+  @override
+  _createnewaccountState createState() => _createnewaccountState();
+}
+
+class _createnewaccountState extends State<createnewaccount> {
+  List<TextEditingController> otpControllers =
+      List.generate(6, (index) => TextEditingController());
+  List<FocusNode> focusNodes = List.generate(6, (index) => FocusNode());
+  bool isMobileNumberEntered = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +23,18 @@ class createnewaccount extends StatelessWidget {
           Container(
             width: 200,
             height: 150,
-            decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(50.0),
-          ),child: Image.asset('assets/whatsapp.png'),
-            
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Image.asset('assets/whatsapp.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25),
+            child: Text(
+              'Login panel',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
           ),
           Container(
             color: Color(0xFFF6F7F8),
@@ -30,43 +49,91 @@ class createnewaccount extends StatelessWidget {
                   padding: const EdgeInsets.only(bottom: 20),
                 ),
                 TextField(
+                  controller: isMobileNumberEntered ? otpControllers[0] : null,
                   decoration: InputDecoration(
                     hintText: 'Mobile number',
                     border: OutlineInputBorder(),
                     labelText: 'Mobile number',
                   ),
                 ),
-              ],
-            ),
-          ),
-          Container(
-            color: Color.fromARGB(255, 245, 246, 245),
-            width: 400,
-            margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'OTP',
-                  ),
-                ),
-                SizedBox(height: 20,width: 50,), // Add spacing between the OTP input and the "Login" button
+                SizedBox(height: 20, width: 50), // Add spacing
                 ElevatedButton(
                   onPressed: () {
-                    // Add your login logic here
+                    setState(() {
+                      isMobileNumberEntered = true;
+                    });
                   },
-                  child: Text('Login'),
+                  child: Text('Submit Mobile Number'),
                 ),
               ],
             ),
           ),
+          if (isMobileNumberEntered)
+            Container(
+              color: Color.fromARGB(255, 245, 246, 245),
+              width: 400,
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(6, (index) {
+                      return Container(
+                        width: 40,
+                        height: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: TextField(
+                          controller: otpControllers[index],
+                          focusNode: focusNodes[index],
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          maxLength: 1,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            counterText: "",
+                          ),
+                          onChanged: (value) {
+                            if (value.isNotEmpty && index < 5) {
+                              focusNodes[index].unfocus();
+                              focusNodes[index + 1].requestFocus();
+                            } else {
+                              focusNodes[index - 1].requestFocus();
+                            }
+                          },
+                        ),
+                      );
+                    }),
+                  ),
+                  SizedBox(
+                      height: 20,
+                      width:
+                          50), // Add spacing between the OTP input and the "Login" button
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your login logic here
+                    },
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
+            ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    for (var controller in otpControllers) {
+      controller.dispose();
+    }
+    super.dispose();
   }
 }
