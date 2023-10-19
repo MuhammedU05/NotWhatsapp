712d253f6673bcaad345fc9d69e4386f3641c7c1
MuhammedU05 <uamuhammed05@gmail.com>
2023-10-19 16:35:52 +0530

    Blue lines

--- lib/Settingspage.dart
+++ lib/Settingspage.dart
@@ -1,4 +1,6 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 class Settingpage extends StatefulWidget {
   const Settingpage({super.key});
@@ -12,11 +14,12 @@ class _SettingpageState extends State<Settingpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 5, 68, 7),
+        toolbarHeight: 60,
+        backgroundColor: themeColorGreen,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("Settings"),
+            const Text("Settings"),
             IconButton(
               iconSize: 35,
               onPressed: () {},
@@ -28,11 +31,11 @@ class _SettingpageState extends State<Settingpage> {
       ),
       body:  SingleChildScrollView(child:  Column(
         children: <Widget>[
-          ListTile(leading: Padding(
+          const ListTile(leading: Padding(
             padding: EdgeInsets.only(right: 23),
             child: CircleAvatar(radius: 60,)
           ),
-          title: Text("Captian Marval"),
+          title: Text("Name"),
           subtitle: Text("hello"),
            
             
@@ -41,95 +44,95 @@ class _SettingpageState extends State<Settingpage> {
             onTap: (){
               
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.key,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'Account',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Security notifications,change number"),
+            subtitle: const Text("Security notifications, Change number"),
           ),
            ListTile(
             onTap: (){
               
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.contact_mail,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'Privacy',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Block contacts,disappearing messages"),
+            subtitle: const Text("Block contacts, Disappearing messages"),
           ), ListTile(
             onTap: (){
               
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.face,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'Avatar',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Create,edit,profile photo"),
+            subtitle: const Text("Create, Edit, Profile photo"),
           ), ListTile(
             onTap: (){
               
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.chat,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'Chats',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Theme,wallpapers,chat history"),
+            subtitle: const Text("Theme, Wallpapers, Chat history"),
           ), ListTile(
             onTap: (){
               
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.notifications,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'Notifications',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Message,group & call tones"),
+            subtitle: const Text("Message, Group & Call tones"),
           ), ListTile(
             onTap: (){
               
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.storage,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'Storage and data',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Network usage,auto-download"),
+            subtitle: const Text("Network usage,auto-download"),
           ), ListTile(
             onTap: (){
 
             },
-            leading: Icon(
+            leading: const Icon(
               Icons.language,
               size: 35,
             ),
-            title: Text(
+            title: const Text(
               'App language',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("English(device's language)"),
-          ), ListTile(
+            subtitle: const Text("English (Device's Language)"),
+          ), const ListTile(
             leading: Icon(
               Icons.help,
               size: 35,
@@ -138,14 +141,14 @@ class _SettingpageState extends State<Settingpage> {
               'Help',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
-            subtitle: Text("Help center,contact us,privacy policy"),
-          ), ListTile(
+            subtitle: Text("Help center, Contact us, Privacy policy"),
+          ), const ListTile(
             leading: Icon(
               Icons.inventory_2_outlined,
               size: 35,
             ),
             title: Text(
-              'Invite a friend',
+              'Invite a Friend',
               style: TextStyle(color: Colors.black, fontSize: 20),
             ),
             
