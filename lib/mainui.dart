c552d62382680f0b28f8d83daa1796d816bc06e2
MuhammedU05 <uamuhammed05@gmail.com>
2023-10-20 10:23:30 +0530

    loginPage

--- lib/mainui.dart
+++ lib/mainui.dart
@@ -4,7 +4,6 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:not_whatsapp/Settingspage.dart';
 import 'package:not_whatsapp/createnewaccount.dart';
 import 'package:not_whatsapp/profilepicpage.dart';
-
 import 'main.dart';
 import 'package:flutter/material.dart';
 import 'chat_screen.dart';
