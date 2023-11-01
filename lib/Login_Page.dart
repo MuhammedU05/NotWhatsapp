642db177bc59b6033751fd100f82dfd45dc1a34a
MuhammedU05 <uamuhammed05@gmail.com>
2023-11-01 13:19:37 +0530

    Account Creation Success

--- lib/Login_Page.dart
+++ lib/Login_Page.dart
@@ -23,7 +23,7 @@ class _EmailSignInScreenState extends State<EmailSignInScreen> {
         print('Successfully signed in with Email and Password');
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => MainApp()),
+          MaterialPageRoute(builder: (context) => const MainApp()),
         );
       }
     } on FirebaseAuthException catch (e) {
@@ -53,8 +53,8 @@ class _EmailSignInScreenState extends State<EmailSignInScreen> {
               'assets/pngwing.com.png',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 25),
+          const Padding(
+            padding: EdgeInsets.only(top: 25),
             child: Text(
               'Login',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
@@ -64,8 +64,8 @@ class _EmailSignInScreenState extends State<EmailSignInScreen> {
             child: Container(
               color: Colors.transparent,
               width: 300,
-              margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
-              padding: EdgeInsets.all(16),
+              margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+              padding: const EdgeInsets.all(16),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -76,12 +76,12 @@ class _EmailSignInScreenState extends State<EmailSignInScreen> {
                     onChanged: (value) {
                       setState(() {});
                     },
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       hintText: 'Enter Email',
                       border: OutlineInputBorder(),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
                   TextField(
@@ -90,24 +90,150 @@ class _EmailSignInScreenState extends State<EmailSignInScreen> {
                     onChanged: (value) {
                       setState(() {});
                     },
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       hintText: 'Enter Password',
                       border: OutlineInputBorder(),
                     ),
                   ),
                   TextButton(
-                    style: ButtonStyle(),
+                    style: const ButtonStyle(),
                     onPressed: () {
                       signInWithEmailAndPassword();
                     },
-                    child: Text('Submit'),
+                    child: const Text('Submit'),
                   ),
-                  SizedBox(height: 40),
+                  const SizedBox(height: 40),
                   TextButton(
                     onPressed: () {
-                      // Implement navigation to the account creation screen
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const CreateAccountScreen()));
                     },
-                    child: Text('Create New Account'),
+                    child: const Text('Create New Account'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CreateAccountScreen extends StatefulWidget {
+  const CreateAccountScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CreateAccountScreenState createState() => _CreateAccountScreenState();
+}
+
+class _CreateAccountScreenState extends State<CreateAccountScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> createAccount() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+      if (userCredential.user != null) {
+        // Navigate to your desired screen upon successful account creation
+        print('Account created successfully');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const EmailSignInScreen()),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Image.asset(
+              'assets/pngwing.com.png',
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 25),
+            child: Text(
+              'Login',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+          Center(
+            child: Container(
+              color: Colors.transparent,
+              width: 300,
+              margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  TextButton(
+                    style: const ButtonStyle(),
+                    onPressed: () {
+                      // signInWithEmailAndPassword();
+                      createAccount();
+                    },
+                    child: const Text('Submit'),
+                  ),
+                  const SizedBox(height: 40),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const EmailSignInScreen()));
+                    },
+                    child: const Text('Login?'),
                   ),
                 ],
               ),
